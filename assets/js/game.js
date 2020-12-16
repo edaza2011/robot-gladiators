6f53a5933d0d92e10e5b8acbbe6e2c08c10c3992
Eric Daza <edazag2011@gmail.com>
2020-12-16 15:34:06 -0600

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots.
+//  *Fight all enemy-robots.
+//  *Defeat each enemy-robot.
+// "LOSE" - Player robot's health is zero or less.
+
 // Create variable playerName and create a window that prompts the user to provide a name for their robot.
 var playerName = window.prompt("What is your robot's name?");
 // Creates starting values for the player's robot and puts some cash in their pocket.
@@ -6,13 +12,40 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 //You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+//console.log(playerName);
+//console.log(playerAttack, playerHealth, playerMoney);
 
-var enemyName = "Roborto";
+//var enemyName = "Roborto";
+// Create an array of enemyNames for multiple combatants; commented out single enemy name line above.
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+
+//Display the array of enemy names in the console:
+//console.log(enemyNames)
+
+//Display each element of the array in the console, one by one:
+//console.log(enemyNames[0]);
+//console.log(enemyNames[1]);
+//console.log(enemyNames[2]);
+
+//Display the number of enemyNames in the enemyNames array.
+//console.log(enemyNames.length)
+
+/*
+//Use a for loop to display the word "apple" three times in the console.
+//The i is the iterator, and this will be logged as well. 
+for(var i= 0; i < 3; i++) {
+    console.log("apple", i);
+};
+*/
+//for(var i = 0; i < enemyNames.length; i++) {
+//    console.log(enemyNames[i]);
+//    console.log(i);
+//    console.log(enemyNames[i] + " is at " + i + " index.");
+//}
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // 1) Alert players that they are starting the round.
     window.alert("Welcome to Robot Gladiators!");
 
@@ -64,16 +97,18 @@ var fight = function() {
             window.alert(playerName + " has decided to skip the fight. Goodbye!");
             // Penalize the player for skipping by subtracting money.
             playerMoney = playerMoney - 2;
-            // Report that the money is actually being lost. 
+            // Report that the money is actually being lost.
             console.log("You dropped some cash scrambling away. You now have $" + playerMoney);
         }
         // If no, (false), ask question again by running fight() again.
         else {
             fight();
         }
-        window.alert(playerName + " has chosen to skip the fight!");
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
 };
-fight();
+// Run fight function to start game.
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
